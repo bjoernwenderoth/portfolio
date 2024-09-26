3fce8890716d8d3a04c85798c221ade3796ace04
Björn Wenderoth <bjoernwe1989@googlemail.com>
2024-09-27 00:11:23 +0200

    structur, color libary, fonts added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
