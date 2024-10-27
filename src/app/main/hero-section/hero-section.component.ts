26b5d8bd928551cce0113dbd34949f9b38c348c7
Björn Wenderoth <bjoernwe1989@googlemail.com>
2024-10-27 16:54:38 +0100

    update

--- src/app/main/hero-section/hero-section.component.ts
+++ src/app/main/hero-section/hero-section.component.ts
@@ -8,31 +8,4 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
 })
-export class HeroSectionComponent implements OnInit, OnDestroy {
-  src: string = '/assets/img/background-images/bow_hero.png';
-  private resizeListener: () => void;
- 
-  constructor() {
-    this.resizeListener = this.onResize.bind(this);
-  }
- 
-  ngOnInit() {
-    this.updateImageSource();
-    window.addEventListener('resize', this.resizeListener);
-  }
- 
-  ngOnDestroy() {
-    window.removeEventListener('resize', this.resizeListener);
-  }
- 
-  private onResize() {
-    this.updateImageSource();
-  }
- 
-  private updateImageSource() {
-    const isMobile = window.innerWidth <= 769;
-    this.src = isMobile
-      ? '/assets/img/background-images/bow_hero_mobile.png'
-      : '/assets/img/background-images/bow_hero.png';
-  }
-}
+export class HeroSectionComponent  {}
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
@@ -23,5 +23,33 @@ import { ImprintComponent } from '../imprint/imprint.component';
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent {}
+export class MainComponent implements OnInit, OnDestroy {
+
+    src: string = '/assets/img/background-images/bow_hero.png';
+    private resizeListener: () => void;
+   
+    constructor() {
+      this.resizeListener = this.onResize.bind(this);
+    }
+   
+    ngOnInit() {
+      this.updateImageSource();
+      window.addEventListener('resize', this.resizeListener);
+    }
+   
+    ngOnDestroy() {
+      window.removeEventListener('resize', this.resizeListener);
+    }
+   
+    private onResize() {
+      this.updateImageSource();
+    }
+   
+    private updateImageSource() {
+      const isMobile = window.innerWidth <= 769;
+      this.src = isMobile
+        ? '/assets/img/background-images/bow_hero_mobile.png'
+        : '/assets/img/background-images/bow_hero.png';
+    }
+  }
 
