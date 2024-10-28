4e47e45b9543515a73ed65caf504566e9096c8cb
Björn Wenderoth <bjoernwe1989@googlemail.com>
2024-10-28 01:13:34 +0100

    update not ready

--- src/app/main/hero-section/hero-section.component.ts
+++ src/app/main/hero-section/hero-section.component.ts
@@ -1,11 +1,30 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
- 
+
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, NgStyle],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
 })
-export class HeroSectionComponent  {}
+export class HeroSectionComponent {
+  isMobile: boolean = false;
+
+  // Check screen size on component initialization
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  // Listen to window resize events
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  // Method to check if the screen width is less than or equal to 768px
+  checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768;
+  }
+}
